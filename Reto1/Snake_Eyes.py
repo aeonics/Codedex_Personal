9dfc4859d1c313b6a08589a8e68ab28ecc1dbc55
Luis R. Alvarez H. <aeonics@users.noreply.github.com>
2025-11-07 23:33:31 -0600

    Snake_Eyes

---
+++ Reto1/Snake_Eyes.py
@@ -0,0 +1,10 @@
+import random
+total = 0
+while total != 2:
+    die1 = random.randint(1 ,6)
+    die2 = random.randint(1 ,6)
+    total = die1+die2
+    if total != 2: 
+        print("reroll the dices")
+    if total == 2:
+        print("Snake Eyes!")
